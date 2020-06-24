4362bdd737e7dbe2a13800d74edb596fd4366839
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-24 13:41:08 -0700

    change text domain

--- app/framework.php
+++ app/framework.php
@@ -5,7 +5,7 @@
  * This file is used to create a new framework instance and adds specific features to the theme.
  *
  * @package     icoptech
- * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @copyright   Copyright (C) 1998-2020. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
@@ -19,15 +19,15 @@ $icoptech = Benlumia007\Backdrop\Framework::get_instance();
 
 $icoptech->menus = new Benlumia007\Backdrop\Menu\Menu(
 	$args = [
-		'primary' => esc_html__( 'Primary Navigation', 'initiator' ),
+		'primary' => esc_html__( 'Primary Navigation', 'icoptech' ),
 	]
 );
 
 $icoptech->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
 	$args = [
 		'primary' => [
-			'name' => esc_html__( 'Primary Sidebar', 'initiator' ),
-			'desc' => esc_html__( 'Test', 'initiator' ),
+			'name' => esc_html__( 'Primary Sidebar', 'icoptech' ),
+			'desc' => esc_html__( 'Test', 'icoptech' ),
 		],
 	]
 );
